85422c21b5b57ac56796153170614d88901ffef3
Kirill Stetsenko <stetsenkoki@gmail.com>
2025-04-25 13:48:25 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <div
+    class="flex h-screen items-center justify-center bg-gradient-to-bl from-blue-700 to-violet-700 text-5xl text-red-500"
+  >
+    Spotify landing.
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
